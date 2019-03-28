f988678bb265f3ebe362ce62ab0579b82717b2df
sharkbruhaha <shark.bruhaha@gmail.com>
2019-03-28 17:22:02 -0400

    added test

--- spec/defines/allow_spec.rb
+++ spec/defines/allow_spec.rb
@@ -7,6 +7,12 @@ describe 'tcpwrappers::allow' do
         let(:title) { 'foo_bar' }
         let(:params) { {:pattern => 'localhost'} }
         it { is_expected.to contain_concat__fragment("tcpwrappers_#{title}") }
+
+        context 'with triggered nets2ddq' do 
+          let(:title) { 'foo_bar_ipv6' }
+          let(:params) { {:pattern => 'localhost 192.168.1.1 2001:0db8:85a3:0000:0000:8a2e:0370:7334' } }
+          it { is_expected.to contain_concat__fragment("tcpwrappers_#{title}") }
+        end
       end
     end
   end
