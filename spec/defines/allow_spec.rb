15033ce260cafaebe4d7bb8f7d4b502a7320cf99
sharkbruhaha <shark.bruhaha@gmail.com>
2019-03-28 19:25:01 -0400

    fixed bad regex in template, and made test

--- spec/defines/allow_spec.rb
+++ spec/defines/allow_spec.rb
@@ -9,9 +9,10 @@ describe 'tcpwrappers::allow' do
         it { is_expected.to contain_concat__fragment("tcpwrappers_#{title}") }
 
         context 'with triggered nets2ddq' do 
-          let(:title) { 'foo_bar_ipv6' }
-          let(:params) { {:pattern => 'localhost 192.168.1.1 2001:0db8:85a3:0000:0000:8a2e:0370:7334' } }
-          it { is_expected.to contain_concat__fragment("tcpwrappers_#{title}") }
+          let(:title) { 'foo_bar' }
+          let(:params) { { :pattern => 'localhost myhost 129.3.0.1 2001:0db8:85a3:0000:0000:8a2e:0370:7334' } }
+          it { is_expected.to contain_concat__fragment("tcpwrappers_#{title}")\
+            .with_content(/localhost,myhost,129.3.0.1,\[2001:0db8:85a3:0000:0000:8a2e:0370:7334\]/) }
         end
       end
     end
