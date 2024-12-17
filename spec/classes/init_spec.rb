7fc4cc09796749b3ad5d8d837e09664cd4cf073d
Steven Pritchard <steve@sicura.us>
2024-12-17 09:36:15 -0600

    Clean up for rubocop

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -4,71 +4,75 @@ describe 'tcpwrappers' do
   context 'supported operating systems' do
     on_supported_os.each do |os, os_facts|
       context "on #{os}" do
-        let (:facts) {
+        let(:facts) do
           os_facts.merge({
-            :interfaces => 'eth0,lo',
-          })
-        }
+                           interfaces: 'eth0,lo',
+                         })
+        end
 
         it { is_expected.to create_class('tcpwrappers') }
         it { is_expected.to contain_package('tcp_wrappers') }
 
         it do
           is_expected.to contain_concat('/etc/hosts.allow').with({
-            'require' => 'Package[tcp_wrappers]'
-          })
+                                                                   'require' => 'Package[tcp_wrappers]'
+                                                                 })
         end
 
         it do
           is_expected.to contain_file('/etc/hosts.deny').with({
-            'owner'   => 'root',
+                                                                'owner' => 'root',
             'group'   => 'root',
             'mode'    => '0644',
             'content' => "ALL: ALL\n",
             'require' => 'Package[tcp_wrappers]'
-          })
+                                                              })
         end
 
         it do
           is_expected.to contain_tcpwrappers__allow('ALL').with({
-            'pattern' => 'LOCAL,foo.example.com,localhost.localdomain,10.0.2.15,127.0.0.1',
+                                                                  'pattern' => 'LOCAL,foo.example.com,localhost.localdomain,10.0.2.15,127.0.0.1',
             'order'   => 0
-          })
+                                                                })
         end
       end
     end
   end
 
   context 'on an unsupported Operating System version' do
-    let(:facts) { {
-      :os => {
-        'name' => 'CentOS',
-        'release' => {
-           'major' => '8',
+    let(:facts) do
+      {
+        os: {
+          'name' => 'CentOS',
+          'release' => {
+            'major' => '8',
+          }
         }
       }
-    } }
+    end
 
-    it 'should not create resources' do
-      is_expected.to_not contain_concat('/etc/hosts.allow')
-      is_expected.to_not contain_package('tcp_wrappers')
+    it 'does not create resources' do
+      is_expected.not_to contain_concat('/etc/hosts.allow')
+      is_expected.not_to contain_package('tcp_wrappers')
     end
   end
 
   context 'on an unsupported Operating System' do
-    let(:facts) { {
-      :os => {
-        'name' => 'Ubuntu',
-        'release' => {
-           'major' => '14',
-           'full'  => '14.999'
+    let(:facts) do
+      {
+        os: {
+          'name' => 'Ubuntu',
+          'release' => {
+            'major' => '14',
+             'full' => '14.999'
+          }
         }
       }
-    } }
+    end
 
-    it 'should not create resources' do
-      is_expected.to_not contain_concat('/etc/hosts.allow')
-      is_expected.to_not contain_package('tcp_wrappers')
+    it 'does not create resources' do
+      is_expected.not_to contain_concat('/etc/hosts.allow')
+      is_expected.not_to contain_package('tcp_wrappers')
     end
   end
 end
